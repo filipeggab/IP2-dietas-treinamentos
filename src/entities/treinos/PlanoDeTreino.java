901b39a8185e941d72ad95c600fd378fd95c0ab6
Filipe Gabriel <filipe.gabriel270726@gmail.com>
2024-12-08 02:46:39 -0300

    inicio da classe PlanoDeTreino

---
+++ src/entities/treinos/PlanoDeTreino.java
@@ -0,0 +1,10 @@
+package entities.treinos;
+
+import java.time.LocalDate;
+
+public abstract class PlanoDeTreino {
+    protected String nome;
+    protected String nivel;
+    protected String objetivo;
+    protected LocalDate dataInicial;
+}
