3e98086d4dd93ee62609385fea93c4a1c76cf8ba
Filipe Gabriel <filipe.gabriel270726@gmail.com>
2024-12-08 00:39:47 -0300

    Correcao da classe Exercicio e preenchimento inicial da classe ExercicioPratico

--- src/entities/treinos/Exercicio.java
+++ src/entities/treinos/Exercicio.java
@@ -5,4 +5,43 @@ public class Exercicio {
     private String descricao;
     private String materiais;
     private String dificuldade;
+
+    public Exercicio(String nome, String descricao, String materiais, String dificuldade) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.materiais = materiais;
+        this.dificuldade = dificuldade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getMateriais() {
+        return materiais;
+    }
+
+    public void setMateriais(String materiais) {
+        this.materiais = materiais;
+    }
+
+    public String getDificuldade() {
+        return dificuldade;
+    }
+
+    public void setDificuldade(String dificuldade) {
+        this.dificuldade = dificuldade;
+    }
 }
---
+++ src/entities/treinos/ExercicioPratico.java
@@ -0,0 +1,25 @@
+package entities.treinos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class ExercicioPratico {
+
+    protected Exercicio exercicio;
+
+    public ExercicioPratico(Exercicio exercicio) {
+        this.exercicio = exercicio;
+    }
+
+    public Exercicio getExercicio() {
+        return exercicio;
+    }
+
+    public void setExercicio(Exercicio exercicio) {
+        this.exercicio = exercicio;
+    }
+
+    public abstract int tempoEstimado();
+    public abstract double percaCalorica();
+
+}
