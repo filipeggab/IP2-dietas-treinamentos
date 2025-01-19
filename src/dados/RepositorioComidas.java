66e57cc1e44677852b4d60b0ecf04c078efbc292
filipeggab <filipe.gabriel270726@gmail.com>
2025-01-19 14:55:52 -0300

    RepositorioComidas feito

---
+++ src/dados/RepositorioComidas.java
@@ -0,0 +1,42 @@
+package dados;
+
+import Exceptions.ComidaNaoCadastradaException;
+import negocio.beans.dietas.Comida;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepositorioComidas {
+    List<Comida> comidas;
+
+    public RepositorioComidas(){
+        this.comidas = new ArrayList<>();
+    }
+
+    public Comida buscarComida(String nome) throws ComidaNaoCadastradaException {
+        Comida comida = comidas.stream().filter(x -> x.getNome().equals(nome)).findFirst().orElse(null);
+        if(comida == null){
+            throw new ComidaNaoCadastradaException(nome);
+        } else{
+            return comida;
+        }
+    }
+    public void criarComida(Comida comida){
+        comidas.add(comida);
+    }
+    public void apagarComida(Comida comida) throws ComidaNaoCadastradaException{
+        if(comidas.contains(comida)){
+            comidas.remove(comida);
+        } else{
+            throw new ComidaNaoCadastradaException(comida.getNome());
+        }
+    }
+    public void apagarComida(String nome) throws ComidaNaoCadastradaException {
+        try{
+            Comida comida = buscarComida(nome);
+            apagarComida(comida);
+        } catch (ComidaNaoCadastradaException e) {
+            throw e;
+        }
+    }
+}
