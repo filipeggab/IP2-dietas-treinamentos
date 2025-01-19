5d3506f746ba375bdb859fd15cd7d9b152ba9059
filipeggab <filipe.gabriel270726@gmail.com>
2025-01-19 11:31:06 -0300

    metodo de RepositorioPlanoDeTreino

--- src/dados/RepositorioPlanoDeTreino.java
+++ src/dados/RepositorioPlanoDeTreino.java
@@ -11,38 +11,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RepositorioPlanoDeTreino {
-    private List<PlanoDeTreino> treinos;
+    private List<PlanoDeTreino> planos;
 
     public RepositorioPlanoDeTreino() {
-        this.treinos = new ArrayList<>();
+        this.planos = new ArrayList<>();
     }
 
     public void adicionarPlanoDeTreino(PlanoDeTreino planoDeTreino) {
-        treinos.add(planoDeTreino);
+        planos.add(planoDeTreino);
     }
     public void removerPlanoDeTreino(PlanoDeTreino planoDeTreino) throws PlanoNaoCadastradoException {
-        if(treinos.contains(planoDeTreino)){
-            treinos.remove(planoDeTreino);
+        if(planos.contains(planoDeTreino)){
+            planos.remove(planoDeTreino);
         }
         else{
             throw new PlanoNaoCadastradoException(planoDeTreino.getNome());
         }
     }
+    public PlanoDeTreino retornarPlanoDeTreino(String nome) throws PlanoNaoCadastradoException{
+        PlanoDeTreino plano = planos.stream().filter(x -> x.getNome().equals(nome)).findFirst().orElse(null);
+        if(plano == null){
+            throw new PlanoNaoCadastradoException(nome);
+        }else{
+            return plano;
+        }
+    }
     public List<PlanoDeTreino> retornarPlanoDeTreinoPorData() {
-        List<PlanoDeTreino> treinosPorData = treinos.stream().filter(x -> x instanceof PlanoDeTreinoPorData).toList();
+        List<PlanoDeTreino> treinosPorData = planos.stream().filter(x -> x instanceof PlanoDeTreinoPorData).toList();
         return treinosPorData;
     }
     public List<PlanoDeTreino> retornarPlanoDeTreinoPorQuantidade() {
-        List<PlanoDeTreino> treinosPorQuantidade = treinos.stream().filter(x -> x instanceof PlanoDeTreinoPorQuantidade).toList();
+        List<PlanoDeTreino> treinosPorQuantidade = planos.stream().filter(x -> x instanceof PlanoDeTreinoPorQuantidade).toList();
         return treinosPorQuantidade;
     }
     public List<PlanoDeTreino> retornarPlanoDeTreinoPorObjetivo(EnumObjetivoDoPlano obj){
-        return treinos.stream().filter(x -> x.getObjetivo() == obj).toList();
+        return planos.stream().filter(x -> x.getObjetivo() == obj).toList();
     }
     public List<PlanoDeTreino> retornarPlanoDeTreinoPorDificuldade(EnumDificuldade diff){
-        return treinos.stream().filter(x -> x.getNivel().equals(diff)).toList();
+        return planos.stream().filter(x -> x.getNivel().equals(diff)).toList();
     }
-    public List<PlanoDeTreino> getTreinos(){
-        return this.treinos;
+    public List<PlanoDeTreino> getPlanos(){
+        return this.planos;
     }
 }
