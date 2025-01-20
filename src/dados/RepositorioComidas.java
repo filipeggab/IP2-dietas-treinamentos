1448f1b387c194c4883e075bc8dbafd2f5f34d1e
Enzo <enzo.veras06@gmail.com>
2025-01-20 10:59:56 -0300

    terminando CadastroDietas

--- src/dados/RepositorioComidas.java
+++ src/dados/RepositorioComidas.java
@@ -9,45 +9,38 @@ import java.util.List;
 public class RepositorioComidas {
     List<Comida> comidas;
 
-    public RepositorioComidas(){
+    public RepositorioComidas() {
         this.comidas = new ArrayList<>();
     }
 
     public Comida buscarComida(String nome) throws ComidaNaoCadastradaException {
         Comida comida = comidas.stream().filter(x -> x.getNome().equals(nome)).findFirst().orElse(null);
-        if(comida == null){
+        if (comida == null) {
             throw new ComidaNaoCadastradaException(nome);
-        } else{
+        } else {
             return comida;
         }
     }
-    public void criarComida(Comida comida){
+
+    public void criarComida(Comida comida) {
         comidas.add(comida);
     }
-    public void apagarComida(Comida comida) throws ComidaNaoCadastradaException{
-        if(comidas.contains(comida)){
+
+    public void apagarComida(Comida comida) throws ComidaNaoCadastradaException {
+        if (comidas.contains(comida)) {
             comidas.remove(comida);
-        } else{
+        } else {
             throw new ComidaNaoCadastradaException(comida.getNome());
         }
     }
+
     public void apagarComida(String nome) throws ComidaNaoCadastradaException {
-        try{
+        try {
             Comida comida = buscarComida(nome);
             comidas.remove(comida);
         } catch (ComidaNaoCadastradaException e) {
             throw e;
         }
-    }
-
-    public void atualizarComida(String nome, Comida novaComida) throws ComidaNaoCadastradaException {
-        for (int i = 0; i < comidas.size(); i++) {
-            if (comidas.get(i).getNome().equalsIgnoreCase(nome)) {
-                comidas.set(i, novaComida);
-                return; // Sai do loop assim que encontrar
-            }
-        }
-        throw new ComidaNaoCadastradaException(nome);
     }
 
 }
