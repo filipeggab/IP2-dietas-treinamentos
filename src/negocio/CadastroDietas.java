785ed9f43027e8be4b7f2d9e9abc9c4d24a13423
Filipe Gabriel <filipe.gabriel270726@gmail.com>
2025-01-20 00:57:55 -0300

    Criação da classe CadastroDietas

---
+++ src/negocio/CadastroDietas.java
@@ -0,0 +1,13 @@
+package negocio;
+
+import dados.RepositorioDietas;
+
+public class CadastroDietas {
+    RepositorioDietas repo;
+
+    public CadastroDietas() {
+        this.repo = new RepositorioDietas();
+    }
+
+
+}
