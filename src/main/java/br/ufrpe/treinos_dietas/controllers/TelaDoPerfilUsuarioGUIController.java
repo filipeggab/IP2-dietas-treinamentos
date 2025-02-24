c287592f2a33f57af4e708c2b2f828dae0d6e5d7
mattiias00 <matheusmatts44@gmail.com>
2025-02-24 14:22:54 -0300

    Aplicando funcao na tela de perfil

--- src/main/java/br/ufrpe/treinos_dietas/controllers/TelaDoPerfilUsuarioGUIController.java
+++ src/main/java/br/ufrpe/treinos_dietas/controllers/TelaDoPerfilUsuarioGUIController.java
@@ -1,13 +1,8 @@
 package br.ufrpe.treinos_dietas.controllers;
-import br.ufrpe.treinos_dietas.Main;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.Parent;
-import javafx.event.ActionEvent;
-import javafx.scene.Node;
+
 import java.io.IOException;
 
 
@@ -18,12 +13,7 @@ public class TelaDoPerfilUsuarioGUIController {
 
     @FXML
     public void btnVoltarTelaPrincipalActionPerformed() throws IOException {
-        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/fxml/TelaPrincipalDoUsuário.fxml"));
-        Parent root = loader.load();
-
-        Stage stage = (Stage) btnVoltarTelaPrincipal.getScene().getWindow();
-        stage.setScene(new Scene(root));
-        stage.show();
+        TelaDoTreinoDaSemanaGUIController.VoltarParaTelaPrincipalDoUsuario(btnVoltarTelaPrincipal );
     }
 }
 
